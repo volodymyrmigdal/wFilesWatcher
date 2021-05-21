a9b342059fe60b7b03a6fb9517d3f85c87a7560f
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-05-21 15:00:26 +0300

    watcher.close

--- proto/wtools/amid/fileswatcher/l1/Namespace.s
+++ proto/wtools/amid/fileswatcher/l1/Namespace.s
@@ -13,10 +13,23 @@ const _ = _global_.wTools;
 const files = _.files = _.files || Object.create( null );
 const Self = files.watcher = files.watcher || Object.create( null );
 
+//
+
+function close()
+{
+  let self = this;
+  let cons = self.watcherArray.map( ( watcher ) => watcher.close() );
+  return _.Consequence.AndKeep( ... cons );
+}
+
+//
+
 let Extension =
 {
   default : null,
-  watcherArray : []
+  watcherArray : [],
+
+  close
 }
 
 _.props.supplement( Self, Extension );
