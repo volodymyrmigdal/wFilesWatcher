e6aa60c08d841e700d4354011bfd661075e4d7f7
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-05-27 18:01:06 +0300

    step script to change system watches limit

---
+++ proto/wtools/amid/l4_files/l7_watcher/step/WatchesLimit.s
@@ -0,0 +1,137 @@
+#! /usr/bin/env node
+( function _WatchesLimit_s_()
+{
+  'use strict';
+
+  const fs = require( 'fs' );
+  const cp = require( 'child_process' );
+  const assert = require( 'assert' );
+
+  const args = process.argv.slice( 2 );
+  const command = args[ 0 ];
+
+  if( command === 'get' )
+  {
+    if( process.platform === 'linux' )
+    getValueLinux();
+    if( process.platform === 'darwin' )
+    getValueDarwin();
+  }
+  else if( command === 'set' )
+  {
+    if( process.platform === 'linux' )
+    setValueLinux();
+    if( process.platform === 'darwin' )
+    setValueDarwin();
+  }
+
+  //
+
+  function getValueLinux()
+  {
+    let currentValue = fs.readFileSync( `/proc/sys/fs/inotify/max_user_watches` );
+    console.log( `Current max_user_watches: ${currentValue}` );
+    return Number( currentValue );
+  }
+
+  //
+
+  function getValueDarwin()
+  {
+    let maxfiles = exec( ` sysctl kern.maxfiles` ).toString();
+    maxfiles = maxfiles.split( /\s+/ )[ 1 ];
+    maxfiles = Number( maxfiles );
+    let maxfilesperproc = exec( ` sysctl kern.maxfilesperproc` ).toString();
+    maxfilesperproc = maxfilesperproc.split( /\s+/ )[ 1 ];
+    maxfilesperproc = Number( maxfilesperproc );
+
+    console.log( `kern.maxfiles: ${maxfiles}` );
+    console.log( `kern.maxfilesperproc: ${maxfilesperproc}` );
+
+    return { current_maxfiles : maxfiles, current_maxfilesperproc : maxfilesperproc }
+  }
+
+  //
+
+  function setValueLinux()
+  {
+    const defaultValue = 1048576;
+    let value = args[ 1 ];
+    const permanent = Boolean( args[ 2 ] );
+
+    getValueLinux();
+
+    value = Number.parseInt( value );
+
+    if( !Number.isInteger( value ) )
+    value = defaultValue;
+
+    console.log( `Changing max_user_watches to ${value}` );
+
+    if( permanent )
+    {
+      exec( `sudo sh -c "echo fs.inotify.max_user_watches=${value} >> /etc/sysctl.conf"` )
+      console.warn( 'The new value is permanent.' );
+      console.warn( 'To disable it edit "fs.inotify.max_user_watches" line in your /etc/sysctl.conf file' );
+    }
+    else
+    {
+      exec( `sudo sysctl fs.inotify.max_user_watches=${value}`)
+      console.warn( 'The new value will persist until next reboot.' )
+    }
+
+    console.log( 'Reloading config file to avoid reboot' );
+    exec( `sudo sysctl -p` );
+
+    getValueLinux();
+  }
+
+  //
+
+  function setValueDarwin()
+  {
+    getValueDarwin();
+
+    let maxfiles = args[ 1 ];
+    let maxfilesperproc = args[ 2 ];
+    const permanent = Boolean( args[ 3 ] );
+    const defaultMaxfiles = 10485760;
+    const defaultMaxfilesperproc = 1048576;
+
+    maxfiles = Number.parseInt( maxfiles );
+    maxfilesperproc = Number.parseInt( maxfilesperproc );
+
+    if( !Number.isInteger( maxfiles ) )
+    maxfiles = defaultMaxfiles;
+
+    if( !Number.isInteger( maxfilesperproc ) )
+    maxfilesperproc = defaultMaxfilesperproc;
+
+    console.log( `Changing maxfiles to ${maxfiles}` );
+    console.log( `Changing maxfilesperproc to ${maxfilesperproc}` );
+
+    if( permanent )
+    {
+      exec( `sudo sh -c "echo kern.maxfiles=${maxfiles} >> /etc/sysctl.conf"` )
+      exec( `sudo sh -c "echo kern.maxfilesperproc=${maxfilesperproc} >> /etc/sysctl.conf"` )
+      console.warn( 'The new values are permanent.' );
+      console.warn( 'To disable it edit lines "kern.maxfiles" and "kern.maxfilesperproc" in your /etc/sysctl.conf file' );
+    }
+    else
+    {
+      exec( `sudo sysctl -w kern.maxfiles=${maxfiles}` )
+      exec( `sudo sysctl -w kern.maxfilesperproc=${maxfilesperproc}` )
+      console.warn( 'The new values will persist until next reboot.' )
+    }
+
+    getValueDarwin();
+  }
+
+  //
+
+  function exec( command )
+  {
+    cp.execSync( command, { stdio : 'inherit' } )
+  }
+
+})()
